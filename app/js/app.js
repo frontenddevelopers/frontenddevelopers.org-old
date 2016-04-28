f58bbfb4b3e4f31416c6961cba2650703dff17c2
Tony Phipps <tonyphipps@gmail.com>
2016-04-28 22:36:22 +0100

    chore: remove old typeform code. Ignore compiled CSS

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-58222334-1', 'auto');
 ga('send', 'pageview');
 
-(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})();
-
 var anchors = document.getElementsByTagName('a');
 
 for (var i = 0; i < anchors.length; i++) {
